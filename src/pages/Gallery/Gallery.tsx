a76e41e9b6776c31cbda5b22985e8aca40c6598f
Ozge Cetin <ozgecetin43@gmail.com>
2024-08-13 18:14:59 +0300

    some components added in gallery and css improvement made

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -4,6 +4,10 @@ import GalleryHero from "../../components/GalleryHero/GalleryHero";
 import ExoticPlaces from "../../components/ExoticPlaces/ExoticPlaces";
 import PlanTrip from "../../components/PlanTrip/PlanTrip";
 import Banner from "../../components/Banner/Banner";
+import Sponsor from "../../components/Sponsor/Sponsor";
+import Subscribe from "../../components/Subscribe/Subscribe";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 
 const Gallery: React.FC = () => {
   return (
@@ -12,6 +16,10 @@ const Gallery: React.FC = () => {
       <ExoticPlaces />
       <PlanTrip />
       <Banner />
+      <Sponsor />
+      <Subscribe />
+      <Testimonials />
+      <WhyChooseUs />
     </>
   );
 };
