4b8d449815a30a778f2bd52cecb1826e2f7f3981
Ozge Cetin <ozgecetin43@gmail.com>
2024-08-13 21:31:51 +0300

    some components added to flight page

--- src/pages/Flights/Flights.tsx
+++ src/pages/Flights/Flights.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import SearchFlight from "../../components/SearchFlight/SearchFlight";
 import BookingFlight from "../../components/BookingFlight/BookingFlight";
+import Map from "../../components/Map/Map";
+import Subscribe from "../../components/Subscribe/Subscribe";
 
 const Flights: React.FC = () => {
   return (
     <>
       <SearchFlight />
       <BookingFlight />
+      <Map />
+      <Subscribe />
     </>
   );
 };
