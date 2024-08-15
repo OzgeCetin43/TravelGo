f144b50c25010e2c815264f2716c56d5bfe227d5
Ozge Cetin <ozgecetin43@gmail.com>
2024-08-15 22:08:44 +0300

    some components added hotel page

--- src/pages/Hotels/Hotels.tsx
+++ src/pages/Hotels/Hotels.tsx
@@ -4,6 +4,9 @@ import SearchHotel from "../../components/SearchHotel/SearchHotel";
 import ExoticPlaces from "../../components/ExoticPlaces/ExoticPlaces";
 import Sponsor from "../../components/Sponsor/Sponsor";
 import BookingHotel from "../../components/BookingHotel/BookingHotel";
+import Services from "../../components/Services/Services";
+import Map from "../../components/Map/Map";
+import Subscribe from "../../components/Subscribe/Subscribe";
 
 const Hotels: React.FC = () => {
   return (
@@ -12,6 +15,9 @@ const Hotels: React.FC = () => {
       <ExoticPlaces />
       <Sponsor />
       <BookingHotel />
+      <Services />
+      <Map />
+      <Subscribe />
     </>
   );
 };
