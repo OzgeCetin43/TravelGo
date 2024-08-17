0fbc1f70fefd949babdb3817c6b29fdbd7dc75f4
Ozge Cetin <ozgecetin43@gmail.com>
2024-08-17 18:02:50 +0300

    some components added to blog page

--- src/pages/Blogs/Blogs.tsx
+++ src/pages/Blogs/Blogs.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 import OurBlog from "../../components/OurBlog/OurBlog";
+import Map from "../../components/Map/Map";
+import Services from "../../components/Services/Services";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import Subscribe from "../../components/Subscribe/Subscribe";
+import Banner from "../../components/Banner/Banner";
 
 const Blogs: React.FC = () => {
   return (
     <>
       <OurBlog />
+      <Map />
+      <Services />
+      <Testimonials />
+      <Subscribe />
+      <Banner />
     </>
   );
 };
